97a5ded589e2a0e73a2442d51dcee70d6cd7d71f
OMATY Divine Marlène <divineomaty@gmail.com>
2026-07-22 20:20:25 +0200

    Ajustement de la responsivite

--- js/main.js
+++ js/main.js
@@ -3,19 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menuToggle');
     const mainNav = document.getElementById('mainNav');
 
-    function checkScroll() {
+    window.addEventListener('scroll', () => {
         if (window.scrollY > 10) {
             siteHeader.classList.add('scrolled');
         } else {
             siteHeader.classList.remove('scrolled');
         }
-    }
+    });
 
-    // Écoute du défilement
-    window.addEventListener('scroll', checkScroll);
-    checkScroll(); // Vérification au chargement
-
-    // Menu burger mobile
     if (menuToggle && mainNav) {
         menuToggle.addEventListener('click', () => {
             mainNav.classList.toggle('open');
